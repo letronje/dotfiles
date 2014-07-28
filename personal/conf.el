5f5a6771b33059addebdf94dcc76d56f1e1ce80e
Manoj Mathai <manojpmathai@gmail.com>
2014-07-28 12:00:19 +0530

    Added keyboard shortcuts removed from prelude

--- personal/conf.el
+++ personal/conf.el
@@ -6,24 +6,25 @@
 (load-file "~/.emacs.d/themes/color-theme-dirac.el")
 (color-theme-dirac)
 
-;(global-whitespace-mode)
-;(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
+;;(global-whitespace-mode)
+;;(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
 
 (set-default-font "Ubuntu Mono:pixelsize=18:foundry=unknown:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
 
 (setq prelude-whitespace nil)
 (setq prelude-guru nil)
 (scroll-bar-mode -1)
-;(setq prelude-flyspell nil)
 
+;;(setq prelude-flyspell nil)
 ;;(setq projectile-enable-caching t)
+
 (setq projectile-completion-system 'grizzl)
 
 (add-hook 'prog-mode-hook
-           (lambda ()
-             (when (> (buffer-size) 40000)
-               (turn-off-smartparens-mode)
-               (turn-off-show-smartparens-mode))))
+          (lambda ()
+            (when (> (buffer-size) 40000)
+              (turn-off-smartparens-mode)
+              (turn-off-show-smartparens-mode))))
 
 (global-linum-mode 1)
 
@@ -46,14 +47,24 @@
 (setq javascript-indent-level 2)
 
 (add-hook 'php-mode-hook
-           (lambda ()
-             (setq indent-tabs-mode nil
-                   tab-width 2
-                   c-basic-offset 2)))
+          (lambda ()
+            (setq indent-tabs-mode nil
+                  tab-width 2
+                  c-basic-offset 2)))
 
 (custom-set-variables '(coffee-tab-width 2))
 
-
+;; Global Keyboard shortcuts
 (global-set-key (kbd "RET") 'newline-and-indent)
+
+(global-set-key (kbd "s-f") 'projectile-find-file)
+(global-set-key (kbd "s-s") 'projectile-switch-project)
+
+(global-set-key (kbd "s-x") 'er/expand-region)
+(global-set-key (kbd "s-z") 'er/contract-region)
+
+(global-set-key (kbd "C-c p a") 'ag-project)
+
+
 
 (provide 'conf)
